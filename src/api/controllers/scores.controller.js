34cf261df2f197fb3fbca1e37895dd633a601fa1
Chintan Shah <chintans98@gmail.com>
2019-03-23 22:12:45 -0400

    Update /scores route to take in date query, output proper transformed info

--- src/api/controllers/scores.controller.js
+++ src/api/controllers/scores.controller.js
@@ -1,10 +1,69 @@
-const { nhlScores } = require('../services/scores.service');
+const { nhlScores, nhlLogo } = require('../services/nhlApi.service');
+
+const getTeamName = (fullName) => {
+  // FIXME: Better handling of getting team name
+  if (fullName.includes("Detroit")) {
+    return "Red Wings";
+  } else if (fullName.includes("Columbus")) {
+    return "Blue Jackets";
+  } else if (fullName.includes("Toronto")) {
+    return "Maple Leafs";
+  } else if (fullName.includes("Vegas")) {
+    return "Golden Knights";
+  } else {
+    return fullName.split(" ").pop();
+  }
+}
+
+const getTeamLogo = (teamId) => {
+  return nhlLogo(teamId);
+}
+
+const transformSingleGame = (game) => {
+  return {
+    gameId: game.gamePk,
+    homeTeam: {
+      teamName: getTeamName(game.teams.home.team.name),
+      fullTeamName: game.teams.home.team.name,
+      teamLogo: getTeamLogo(game.teams.home.team.id)
+    },
+    roadTeam: {
+      teamName: getTeamName(game.teams.away.team.name),
+      fullTeamName: game.teams.away.team.name,
+      teamLogo: getTeamLogo(game.teams.away.team.id)
+    },
+    gameInformation: {
+      gameStatus: game.status.abstractGameState, // either "Live", "Preview", "Final"
+      gameDate: game.gameDate,
+      periodInfo: {
+        currentPeriod: game.linescore.currentPeriodOrdinal,
+        currentPeriodTimeRemaining: game.linescore.currentPeriodTimeRemaining
+      }
+    },
+    gameScore: {
+      homeTeam: game.teams.home.score,
+      roadTeam: game.teams.away.score
+    }
+  }
+}
+
+const transformNhlScores = (scores) => {
+  const { date, games } = scores.schedule;
+  const formattedGames = games.map((game) => transformSingleGame(game));
+
+  return {
+    scores: {
+      date: date,
+      games: formattedGames
+    }
+  }
+}
 
 const getScores = async (req, res, next) => {
-  const scores = await nhlScores();
-
   try {
-    return res.json(scores);
+    const rawScores = await nhlScores(req.query);
+
+    return res.json(transformNhlScores(rawScores));
   } catch (error) {
     return next(error);
   }
--- src/api/validations/scores.validation.js
+++ src/api/validations/scores.validation.js
@@ -3,5 +3,8 @@ const Joi = require('joi');
 module.exports = {
   // GET /v1/scores
   scoresReq: {
+    query: {
+      date: Joi.date().iso().default(() => new Date(), 'current date')
+    }
   }
 };
