a73f5310ec282c24a8c7ed6d5c669089d4c86988
Chintan Shah <chintans98@gmail.com>
2019-03-23 22:12:16 -0400

    Add util class for date formatting
    - to format date to YYYY-MM-DD format

---
+++ src/api/utils/date.util.js
@@ -0,0 +1,11 @@
+
+/**
+ * Return formatted date string in YYYY-MM-DD format
+ *
+ * @param {Date} date
+ */
+const dateFormatter = (date) => {
+  return date.toISOString().split('T')[0];
+}
+
+module.exports = { dateFormatter };
