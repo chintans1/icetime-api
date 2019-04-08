7c25d42d09182d886f8c2877bb747ce8b4de4a46
Chintan Shah <chintans98@gmail.com>
2019-04-08 16:52:34 -0400

    remove everything related to /teams route
    - not needed anymore or atleast the logo stuff isn't needed

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const gamesRoutes = require('./games.route');
-const teamsRoutes = require('./teams.route');
 
 const router = express.Router();
 
@@ -13,10 +12,5 @@ router.get('/status', (req, res) => res.send('OK'));
  * GET v1/games
  */
 router.use('/games', gamesRoutes);
-
-/**
- * GET v1/teams
- */
-router.use('/teams', teamsRoutes);
 
 module.exports = router;
