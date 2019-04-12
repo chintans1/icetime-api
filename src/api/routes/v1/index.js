727ef2d709637e676f4cc56523b7d3a9fa52bfa2
Chintan Shah <chintans98@gmail.com>
2019-04-11 20:18:44 -0400

    Don't need req var for /status

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 /**
  * GET v1/status
  */
-router.get('/status', (req, res) => res.send('OK'));
+router.get('/status', (_, res) => res.json('OK'));
 
 /**
  * GET v1/games
